d8cf04675f9352a33ca9674e2ebc2edbb391852d
philip <me@filou.se>
2019-08-27 11:03:51 +0200

    added include/dlsproprietarychunkids.h

---
+++ include/dlsproprietarychunkids.h
@@ -0,0 +1,20 @@
+/**
+ * \file
+ *
+ * @url https://www.midi.org/specifications-old/item/dls-proprietary-chunk-ids
+ */
+
+#ifndef MIDIMESSAGE_DLSPROPRIETARYCHUNKIDS_H
+#define MIDIMESSAGE_DLSPROPRIETARYCHUNKIDS_H
+
+namespace MidiMessage {
+
+    const char DlsProprietaryChunkIdMidiManufacturersAssociation[]  = "MMA_";
+    const char DlsProprietaryChunkIdEssTechnology[]                 = "ESS_";
+    const char DlsProprietaryChunkIdCrystalSemiconductor[]          = "CRS_";
+    const char DlsProprietaryChunkIdYamahaCorporation[]             = "YMH_";
+    const char DlsProprietaryChunkIdSoniEricsson[]                  = "SEM_";
+
+}
+
+#endif //MIDIMESSAGE_DLSPROPRIETARYCHUNKIDS_H
